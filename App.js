a570e7a6f454ebfe5aa5fcaf0d228dbe7624f652
bratati57 <bratati7878@gmail.com>
2025-05-21 10:03:13 +0530

    Episode2NPM,Parcel,browserslist

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from "react"
+import ReactDOM from 'react-dom/client';
 const parent = React.createElement("div", { id: "parent", key: "lala" },
-    [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", { id: "heading1" }, "I am a h1 tag"),
-        React.createElement("h2", { id: "heading2" }, "I am a h2 tag")]),
-    React.createElement("div", { id: "child2" },
-        [React.createElement("h1", { id: "heading3" }, "I am a h1 tag"),
-        React.createElement("h2", { id: "heading4" }, "I am a h2 tag")])])
+    [React.createElement("div", { id: "child1", key:"12" },
+        [React.createElement("h1", { id: "heading1", key:"13"  }, "I am a h1 tag"),
+        React.createElement("h2", { id: "heading2", key:"14" }, "I am a h2 tag")]),
+    React.createElement("div", { id: "child2", key:"15" },
+        [React.createElement("h1", { id: "heading3", key:"16"  }, "I am a h1 tag"),
+        React.createElement("h2", { id: "heading4", key:"17" }, "I am a h2 tag")])])
 console.log(parent)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
