bce912b71424dc1a3bdeb0da08f7809f8eac31c8
bratati57 <bratati7878@gmail.com>
2025-06-05 14:43:03 +0530

    epi9-classComponentDidMountUpdateWillUnmount

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,39 @@
-import UserClass from "./UserClass"
-import User from "./User"
+import UserClass from "./UserClass";
+// import User from "./User"
+import React from "react";
 
-const About=()=>{
-    return(
-        <div>
-            <h1>PeTook</h1>
-            <h2>This is a food ordering App built while learning from Namaste React Course</h2>
-            <User name="Bratati" from="Agartala"/>
-            <UserClass name="Bratati Class" from="Agartala class"/>
-            </div>
-    )
+class About extends React.Component {
+  constructor() {
+    super()
+    console.log("==parent constructor");
+  }
+   componentDidMount() {
+    //API calls made here
+    this.timer=setInterval(()=>{
+        console.log("set interval 1 sec")
+    },[1000])
+    console.log("==parent component did mount");
+  }
+  componentDidUpdate() {
+    console.log("==parent component did update");
+  }
+  componentWillUnmount(){
+    clearInterval(this.timer)
+    console.log("==parent unmounted")
+  }
+  render() {
+    console.log("==parent render");
+    return (
+      <div>
+        <h1>PeTook</h1>
+        <h2>
+          This is a food ordering App built while learning from Namaste React
+          Course
+        </h2>
+        {/* <User name="Bratati" from="Agartala"/> */}
+        <UserClass name="Bratati Class" from="Agartala class" />
+      </div>
+    );
+  }
 }
-export default About
+export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,20 +2,34 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-        count:0,
-        count2:2,
-    }
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
+    console.log("child constructor");
+  }
+  componentDidMount() {
+    console.log("child component did mount");
+  }
+  componentDidUpdate() {
+    console.log("child component did update");
+  }
+  componentWillUnmount(){
+    console.log("child unmounted")
   }
   render() {
-    const {name, from}=this.props;
-    const {count}=this.state;
+    const { name, from } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-card">
         <h1>Count: {count}</h1>
-        <button onClick={()=>{
-            this.setState({count:this.state.count+1})
-        }}>Increase Count</button>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increase Count
+        </button>
         <h1>{name}</h1>
         <h2>{from}</h2>
         <h3>bratati7878@gmail.com</h3>
